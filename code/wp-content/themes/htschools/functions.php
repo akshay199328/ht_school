5605d705e4abe0a7b199a0ae8e22a18b4aa8e807
priyankaw-1995 <74304751+priyankaw-1995@users.noreply.github.com>
2021-03-26 15:40:23 +0530

    changes

--- code/wp-content/themes/htschools/functions.php
+++ code/wp-content/themes/htschools/functions.php
@@ -110,7 +110,7 @@ wp_enqueue_style( 'wplms-font', get_template_directory_uri(). '/assets/css/font.
 wp_enqueue_style( 'wplms-owl-carousel', get_template_directory_uri(). '/assets/css/owl.carousel.min.css' );
 wp_enqueue_style( 'wplms-navigation', get_template_directory_uri(). '/navigation.css' );
 // wp_enqueue_script( 'wplms-aos-js', get_template_directory_uri(). '/assets/vendor/aos/aos.js', '', '', true );
-// wp_enqueue_script( 'wplms-jquery', get_template_directory_uri(). '/assets/js/jquery.min.js', '', '', true );
+wp_enqueue_script( 'wplms-jquery', get_template_directory_uri(). '/assets/js/jquery.min.js', '', '', true );
 wp_enqueue_script( 'wplms-carousel', 'https://cdn.boomcdn.com/libs/owl-carousel/2.3.4/owl.carousel.min.js', '', '', true );
 // wp_enqueue_script( 'wplms-owl', get_template_directory_uri(). '/assets/js/owl-carousel.min.js', '', '', true );
 wp_enqueue_script( 'wplms-main-js', get_template_directory_uri(). '/assets/js/main.js', '', '', true );

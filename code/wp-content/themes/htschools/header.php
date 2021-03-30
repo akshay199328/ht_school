57d085f9b86089dbdb39c84b934cfdb5db374556
bhavna <bhavna.langi@fortune4.in>
2021-03-30 13:51:52 +0530

    UI Changes

--- code/wp-content/themes/htschools/header.php
+++ code/wp-content/themes/htschools/header.php
@@ -154,7 +154,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
                    $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
                    $menuitems = wp_get_nav_menu_items( $menu->term_id, array( 'order' => 'DESC' ) );
                    //print_r($menuitems);
-                   echo "<ul><li class='search-icon'><a id='new_searchicon'><i class='bi bi-search'></i></a></li>";
+                   echo "<ul><li class='search-icon'><a id='new_searchicon'><img src=".get_bloginfo('template_url')."/assets/images/search.svg></a></li>";
                    if (is_user_logged_in()){
                        do_action('woocommerce_add_to_cart_fragments'); 
                     }

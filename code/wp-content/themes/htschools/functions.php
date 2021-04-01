067d4dca361a0b65e2e7eff3dc334e17aae711ac
sameer <sameer@fortune4.in>
2021-04-01 13:51:59 +0530

    course

--- code/wp-content/themes/htschools/functions.php
+++ code/wp-content/themes/htschools/functions.php
@@ -785,3 +785,16 @@ function get_the_term_list_search( $post_id, $taxonomy, $before = '', $sep = '',
 
   return $before . implode( $sep, $term_links ) . $after;
 }
+
+
+
+add_filter( 'my_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2 );
+function add_extra_item_to_nav_menu( $items, $args ) {
+    if (is_user_logged_in() && $args->menu == 303) {
+        $items .= '<li><a href="'. get_permalink( get_option('woocommerce_myaccount_page_id') ) .'">My Account</a></li>';
+    }
+    elseif (!is_user_logged_in() && $args->menu == 303) {
+        $items .= '<li><a href="' . get_permalink( wc_get_page_id( 'myaccount' ) ) . '">Sign in  /  Register</a></li>';
+    }
+    return $items;
+}

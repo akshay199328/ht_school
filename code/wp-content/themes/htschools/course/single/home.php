71e7d7a89d5fce97158512b349652b225429e4d6
Prashant <teamleados@uppercliff.com-at-078010589169>
2021-04-05 16:47:41 +0530

    condition for course save, share options added in news page from the course page

--- code/wp-content/themes/htschools/course/single/home.php
+++ code/wp-content/themes/htschools/course/single/home.php
@@ -146,6 +146,7 @@ $course_curriculum = ht_course_get_full_course_curriculum($id);
                                   <li class="nav-item">
                                     <a class="nav-link scrollto" href="#instructor">Instructor</a>
                                 </li>
+
                                 <div class="share-icon">
                                     <a href="https://www.addtoany.com/share" class="a2a_dd"><svg xmlns="" width="16" height="16" fill="currentColor" class="bi bi-share" viewBox="0 0 16 16">
                                       <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z"></path>
@@ -153,6 +154,19 @@ $course_curriculum = ht_course_get_full_course_curriculum($id);
                                      <script async src="https://static.addtoany.com/menu/page.js"></script>
                                   </div>
                               </ul>
+                              <div class="top-icon">
+                               <?php
+                               if(is_user_logged_in()){
+                                ?>
+                                <li style="list-style-type: none;"><?php wpfp_link(); ?></li>
+                              <?php }else{
+                                $url = "/login-register";
+                                ?>
+                                <li style="list-style-type: none;"><a href="<?php echo get_site_url().$url; ?>"><i class="bi bi-bookmark"></i></a></li> 
+                                <?php
+                              }
+                              ?>
+                            </div>
                           </div>
                         </div>
                       </div>

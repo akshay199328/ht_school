67f6b1911a9029ac0046698f0c822e6ad90dbab1
sameer <sameer@fortune4.in>
2021-04-06 18:21:12 +0530

    scroll issue

--- code/wp-content/themes/htschools/assets/js/navigation-custom.js
+++ code/wp-content/themes/htschools/assets/js/navigation-custom.js
@@ -61,3 +61,11 @@ $(document).ready(function(){
   });
 
 });
+
+  $(".scrollTo").on('click', function(e) {
+     e.preventDefault();
+     var target = $(this).attr('href');
+     $('html, body').animate({
+       scrollTop: ($(target).offset().top -170)
+     });
+  });

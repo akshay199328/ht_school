0f42a95b1a4071586da6b36dce9c6802de791536
bhavna <bhavna.langi@fortune4.in>
2021-04-08 12:35:56 +0530

    UI Change

---
+++ code/wp-content/themes/htschools/static-pages.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Template Name: Static Pages
+ */
+
+get_header(vibe_get_header());
+if ( have_posts() ) : while ( have_posts() ) : the_post();
+
+$title=get_post_meta(get_the_ID(),'vibe_title',true);
+if(vibe_validate($title) || empty($title)){
+?>
+<section id="title">
+    <div class="<?php echo vibe_get_container(); ?>">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="pagetitle">
+                    <?php
+                        $breadcrumbs=get_post_meta(get_the_ID(),'vibe_breadcrumbs',true);
+                        if(vibe_validate($breadcrumbs) || empty($breadcrumbs))
+                            vibe_breadcrumbs(); 
+                    ?>
+                    <h1><?php the_title(); ?></h1>
+                    <?php the_sub_title(); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+}
+?>
+<section id="content">
+    <div class="<?php echo vibe_get_container(); ?>">
+        <div class="row">
+            <div class="col-md-3 col-sm-4">
+                <div class="sidebar">
+                   <ul class="border-link">
+                        <?php
+                           $menu_name = 'footer-menu'; //menu slug
+                           $locations = get_nav_menu_locations();
+                           $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
+                           $menuitems = wp_get_nav_menu_items( $menu->term_id, array( 'order' => 'DESC' ) );
+                           //print_r($menuitems);
+                           foreach ($menuitems as $menu) {  
+                            if($menu->title != 'Contact Us'){
+                               echo '<li><a href="' . $menu->url . '">' . $menu->title . '</a></li>';
+                            }
+                           }
+                       ?>
+
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-9 col-sm-8">
+                <div class="content">
+                    <?php
+                        the_content();
+                       
+                     ?>
+                </div>
+                <?php
+                
+                endwhile;
+                endif;
+                ?>
+            </div>
+     
+        </div>
+    </div>
+</section>
+
+<?php
+get_footer(vibe_get_footer());
+?>

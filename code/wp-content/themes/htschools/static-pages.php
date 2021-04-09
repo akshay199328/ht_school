06e558eb994e76ad94b30046d2db9a2772e31fb4
bhavna <bhavna.langi@fortune4.in>
2021-04-09 16:53:51 +0530

    Static pages

--- code/wp-content/themes/htschools/static-pages.php
+++ code/wp-content/themes/htschools/static-pages.php
@@ -9,62 +9,63 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
 $title=get_post_meta(get_the_ID(),'vibe_title',true);
 if(vibe_validate($title) || empty($title)){
 ?>
-<section id="title">
-    <div class="<?php echo vibe_get_container(); ?>">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="pagetitle">
-                    <?php
-                        $breadcrumbs=get_post_meta(get_the_ID(),'vibe_breadcrumbs',true);
-                        if(vibe_validate($breadcrumbs) || empty($breadcrumbs))
-                            vibe_breadcrumbs(); 
-                    ?>
-                    <h1><?php the_title(); ?></h1>
-                    <?php the_sub_title(); ?>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+<div class="innerheader-space"></div>
+
 <?php
 }
 ?>
-<section id="content">
+<section id="content" class="static_content grey-background">
     <div class="<?php echo vibe_get_container(); ?>">
         <div class="row">
-            <div class="col-md-3 col-sm-4">
-                <div class="sidebar">
-                   <ul class="border-link">
-                        <?php
-                           $menu_name = 'footer-menu'; //menu slug
-                           $locations = get_nav_menu_locations();
-                           $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
-                           $menuitems = wp_get_nav_menu_items( $menu->term_id, array( 'order' => 'DESC' ) );
-                           //print_r($menuitems);
-                           foreach ($menuitems as $menu) {  
-                            if($menu->title != 'Contact Us'){
-                               echo '<li><a href="' . $menu->url . '">' . $menu->title . '</a></li>';
-                            }
-                           }
-                       ?>
+            <div class="col-md-12 ">
+              <div class="breadcrumbs">
+                  <?php
+                      $breadcrumbs=get_post_meta(get_the_ID(),'vibe_breadcrumbs',true);
+                      if(vibe_validate($breadcrumbs) || empty($breadcrumbs))
+                          vibe_breadcrumbs(); 
+                  ?>
+                  
+                  <?php the_sub_title(); ?>
+              </div>
+            </div>
+            <div class="details">
+                <div class="col-md-3 col-sm-4">
+                    <div class="sidebar">
+                       <ul class="border-link">
+                            <?php
+                               $menu_name = 'footer-menu'; //menu slug
+                               $locations = get_nav_menu_locations();
+                               $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
+                               $menuitems = wp_get_nav_menu_items( $menu->term_id, array( 'order' => 'DESC' ) );
+                               //print_r($menuitems);
+                               foreach ($menuitems as $menu) {  
+                                if($menu->title != 'Contact Us'){
+                                   echo '<li><a href="' . $menu->url . '">' . $menu->title . '</a></li>';
+                                }
+                               }
+                           ?>
 
-                    </ul>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-            <div class="col-md-9 col-sm-8">
-                <div class="content">
+                <div class="col-md-9 col-sm-8">
+                    <div class="content">
+                      <div class="pagetitle">
+                        <h1><?php the_title(); ?></h1> 
+                      </div>
+                      
+                        <?php
+                            the_content();
+                           
+                         ?>
+                    </div>
                     <?php
-                        the_content();
-                       
-                     ?>
+                    
+                    endwhile;
+                    endif;
+                    ?>
                 </div>
-                <?php
-                
-                endwhile;
-                endif;
-                ?>
             </div>
-     
         </div>
     </div>
 </section>

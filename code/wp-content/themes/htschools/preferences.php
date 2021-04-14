8d55962b287201df03abf918b03b70929462efa6
bhavna <bhavna.langi@fortune4.in>
2021-04-14 17:08:40 +0530

    UI Changes

--- code/wp-content/themes/htschools/preferences.php
+++ code/wp-content/themes/htschools/preferences.php
@@ -30,7 +30,7 @@ vibe_include_template("profile/top$profile_layout.php");
             <div class="col-md-12 col-sm-12 profile_searchbox mrg">
                 <form action="" id="search-form" method="get">
                     
-                    <?php acf_form();?>
+                    <!-- <?php acf_form();?> -->
                     <div class="search_value">
                     <?php 
                         $user = wp_get_current_user();
@@ -58,7 +58,7 @@ vibe_include_template("profile/top$profile_layout.php");
 
                     ?>
                     </div>
-                   <div class="search_section">
+                   <!-- <div class="search_section">
                         <div class="search_icon"></div>
                         <input type="text" name="s" id="s" placeholder="Find an Interest to add" onblur="if(this.value=='')this.placeholder='Find an Interest to add'" onfocus="if(this.placeholder=='Find an Interest to add')this.placeholder=''">
                         <input type="hidden" value="submit">
@@ -73,7 +73,7 @@ vibe_include_template("profile/top$profile_layout.php");
                         <span><a href="#">CSS3 <i class="bi bi-x"></i></a></span>
                         <span><a href="#">CSS3 <i class="bi bi-x"></i></a></span>
                         <span><a href="#">Javascript <i class="bi bi-x"></i></a></span>
-                    </div>
+                    </div> -->
                 </form>
                 
             </div>

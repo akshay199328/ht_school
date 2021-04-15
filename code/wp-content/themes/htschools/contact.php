36005c0d51a9bf74fbd3017ab1a0adf6ed86bcbd
bhavna <bhavna.langi@fortune4.in>
2021-04-15 12:55:06 +0530

    Contact change

--- code/wp-content/themes/htschools/contact.php
+++ code/wp-content/themes/htschools/contact.php
@@ -34,6 +34,31 @@ if(have_posts()):while(have_posts()):the_post();
             </div>
         </div>
     </div>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+<style>
+  .modal-footer .btn{
+    
+  }
+</style>
 </section>
 <?php
   $protocol = is_ssl() ? 'https' : 'http';
@@ -48,7 +73,10 @@ if(have_posts()):while(have_posts()):the_post();
 <script type='text/javascript'>
   let baseUrl = "<?php echo get_home_url(); ?>"
   document.addEventListener( 'wpcf7mailsent', function( event ) {
-    location = baseUrl + '/thank-you';
+   if ( '1024' == event.detail.contactFormId ) { // Change 123 to the ID of the form 
+  
+    jQuery('#exampleModal').modal('show'); 
+ }
   }, false );
 
   var map;

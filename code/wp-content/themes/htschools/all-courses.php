5433680aafb86ba74b07fb8aa110f3bf2ea651cb
bhavna <bhavna.langi@fortune4.in>
2021-04-20 12:23:51 +0530

    UI Changes

--- code/wp-content/themes/htschools/all-courses.php
+++ code/wp-content/themes/htschools/all-courses.php
@@ -7,38 +7,38 @@ if ( !defined( 'ABSPATH' ) ) exit;
 get_header(vibe_get_header());
 ?>
 <main id="main">
-	<section class="breadcrumbs background-breadcrumbs">
+  <section class="breadcrumbs background-breadcrumbs">
       <div class="innerheader-space"></div>
       <div class="container">
         <?php
-        	$breadcrumbs=get_post_meta($id,'vibe_breadcrumbs',true);
+          $breadcrumbs=get_post_meta($id,'vibe_breadcrumbs',true);
             if(!isset($breadcrumbs) || !$breadcrumbs || vibe_validate($breadcrumbs)){
                 vibe_breadcrumbs();
             }
-        	echo '<h2>'.vibe_get_title($id).'</h2>';
-	        the_sub_title($id);
+          echo '<h2>'.vibe_get_title($id).'</h2>';
+          the_sub_title($id);
         ?>
       </div>
     </section>
     <section id="Popular-Courses" class="all_course_page">
       <div class="container aos-init aos-animate" data-aos="fade-up">
         <div class="row">
-          <div class="col-lg-8 mrg">
+          <div class="col-sm-12 col-md-9 col-lg-9 mrg">
             <div class="">
                 <div class="col-md-12 mrg space" data-aos="zoom-out" data-aos-delay="200">
-                	<?php
-		                $args_course = array(
-		                    'post_type' => 'course',
-		                    'post_status' => 'publish',
-                        'posts_per_page'=>12,
-		                );
-		                $Query_course = new WP_Query( $args_course );
-		                if ($Query_course->have_posts()) : while ($Query_course->have_posts()) : $Query_course->the_post();
-		                  $custom_fields = get_post_custom();
-		                  $duration = $custom_fields['vibe_duration'][0];
+                  <?php
+                    $args_course = array(
+                        'post_type' => 'course',
+                        'post_status' => 'publish',
+                        'posts_per_page'=>20,
+                    );
+                    $Query_course = new WP_Query( $args_course );
+                    if ($Query_course->have_posts()) : while ($Query_course->have_posts()) : $Query_course->the_post();
+                      $custom_fields = get_post_custom();
+                      $duration = $custom_fields['vibe_duration'][0];
                       $session = $custom_fields['vibe_course_sessions'][0];
-		                  $age_limit = $custom_fields['vibe_course_age_group'][0];
-		                  $category_array = get_the_terms( $post->ID, 'course-cat');
+                      $age_limit = $custom_fields['vibe_course_age_group'][0];
+                      $category_array = get_the_terms( $post->ID, 'course-cat');
                       $excerpt = get_post_field('post_excerpt', $post->ID);
                   ?>
                   <div class="course-box dotted-border">
@@ -53,8 +53,8 @@ get_header(vibe_get_header());
                     <div class="col-xs-10 col-sm-10 col-lg-10 pull-left mrg">
                       <div class="col-sm-12 col-lg-9 pull-left mrg">
                         <div class="middle-details">
-                          	<h6><?php echo $category_array[0]->name; ?></h6>
-                        	<h2><?php echo bp_course_title(); ?></h2>
+                            <h6><?php echo $category_array[0]->name; ?></h6>
+                          <h2><?php echo bp_course_title(); ?></h2>
                           <div class="col-lg-12 share-icon mrg">
                             <i class="bi bi-share"></i>
                           </div>
@@ -71,12 +71,12 @@ get_header(vibe_get_header());
                               <?php }?>
                             </div>
                             <div class="pull-right">
-	                            <p>Age Group</p>
-	                            <?php if($age_limit == '') { ?>
-	                            <h6>--</h6>
-	                            <?php } else{ ?>
-	                            <h6><?php echo $age_limit;?></h6>
-	                            <?php }?>
+                              <p>Age Group</p>
+                              <?php if($age_limit == '') { ?>
+                              <h6>--</h6>
+                              <?php } else{ ?>
+                              <h6><?php echo $age_limit;?></h6>
+                              <?php }?>
                             </div>
                           </div>
                         </div>
@@ -119,7 +119,7 @@ get_header(vibe_get_header());
                 </div>
             </div>
         </div>
-        <div class="col-lg-4 mrg adworks desktop-add right-adwork">
+        <div class="col-sm-12 col-md-3 col-lg-3 mrg adworks desktop-add right-adwork right_spacing">
             <?php
               if ( is_active_sidebar( 'banner-1' ) ) : ?>
               <?php dynamic_sidebar( 'banner-1' ); ?>      
@@ -129,11 +129,11 @@ get_header(vibe_get_header());
 </div>
 </section>
 <div class="col-md-12 col-sm-12">
-		<?php
-			$sidebar = apply_filters('wplms_sidebar','competitive-section');
+    <?php
+      $sidebar = apply_filters('wplms_sidebar','competitive-section');
             if ( !function_exists('dynamic_sidebar')|| !dynamic_sidebar($sidebar) ) : ?>
-       	<?php endif; ?>
-	</div>
+        <?php endif; ?>
+  </div>
 </main>
 <?php
 get_footer(vibe_get_footer());

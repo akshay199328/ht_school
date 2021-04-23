07b079b399edc73aa0c3aab05ce76e8c33b06f05
swati khote <swakhopoli128@gmail.com>
2021-04-23 22:29:06 +0530

    added css and apply classes on index page

--- code/wp-content/themes/htschools/templates/directory/course/index.php
+++ code/wp-content/themes/htschools/templates/directory/course/index.php
@@ -44,7 +44,7 @@ if(!isset($title) || !$title || (vibe_validate($title))){
 <?php
 }
 ?>
-<section id="Popular-Courses">
+<section id="Popular-Courses" clas="all_course_page">
 	<!-- <div id="buddypress"> -->
     	<div class="<?php echo vibe_get_container(); ?>">
 			<?php do_action( 'bp_before_directory_course_page' ); ?>

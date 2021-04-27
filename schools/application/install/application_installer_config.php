ac18263d53a93736168972025532490c18033b94
sameer <sameer@fortune4.in>
2021-04-27 16:58:30 +0530

    remove install directory

--- schools/application/install/application_installer_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$app_name = 'iBilling';
-$app_url = 'www.cloudonex.com';
-$release_date = 'Oct 30, 2018';
-$envato_username = 'CloudOnex';
-$author_name = 'CloudOnex';
-$support_url = 'https://www.cloudonex.com';

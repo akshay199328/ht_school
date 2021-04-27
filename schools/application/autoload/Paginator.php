05686698d7ede2555b31aebc009e5202138362ad
sameer <sameer@fortune4.in>
2021-04-27 18:21:43 +0530

    schools

--- schools/application/autoload/Paginator.php
+++ schools/application/autoload/Paginator.php
@@ -1,23 +1,5 @@
 <?php
-// *************************************************************************
-// *                                                                       *
-// * iBilling -  Accounting, Billing Software                              *
-// * Copyright (c) Sadia Sharmin. All Rights Reserved                      *
-// *                                                                       *
-// *************************************************************************
-// *                                                                       *
-// * Email: sadiasharmin3139@gmail.com                                                *
-// * Website: http://www.sadiasharmin.com                                  *
-// *                                                                       *
-// *************************************************************************
-// *                                                                       *
-// * This software is furnished under a license and may be used and copied *
-// * only  in  accordance  with  the  terms  of such  license and with the *
-// * inclusion of the above copyright notice.                              *
-// * If you Purchased from Codecanyon, Please read the full License from   *
-// * here- http://codecanyon.net/licenses/standard                         *
-// *                                                                       *
-// *************************************************************************
+
 Class Paginator
 {
     public static function bootstrap($table, $w1='',$c1='', $w2='', $c2= '', $w3='',$c3='', $w4='', $c4= '', $per_page = '50')

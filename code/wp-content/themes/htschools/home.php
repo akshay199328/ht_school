0ec1fdf2cc92fe4c6973a76c9b3ab229682c795d
swati khote <swakhopoli128@gmail.com>
2021-05-06 20:53:05 +0530

    add advertised section

--- code/wp-content/themes/htschools/home.php
+++ code/wp-content/themes/htschools/home.php
@@ -61,118 +61,121 @@ if ( is_active_sidebar( 'home-hero-section' ) ) : ?>
   <div class="container" data-aos="fade-up">
     <div class="row">
       <div class="col-sm-12 col-lg-4 mrg">
-       <header class="section-header">
-        <h2>TRENDING COURSES</h2> 
-        <p>Access the world’s activities, anytime anywhere.</p>
-        <a href="<?php echo get_home_url();?>/courses/" class="exlore-link desktop-btn">Discover All Courses</a>
-      </header>
-      <div class="adworks">
-        <?php
-        if ( is_active_sidebar( 'homepage-mid-banner' ) ) : ?>
-          <?php dynamic_sidebar( 'homepage-mid-banner' ); ?>      
-        <?php endif; ?>
+          <header class="section-header">
+              <h2>TRENDING COURSES</h2> 
+              <p>Access the world’s activities, anytime anywhere.</p>
+              <a href="<?php echo get_home_url();?>/courses/" class="exlore-link desktop-btn">Discover All Courses</a>
+          </header>
+          <div class="adworks">
+            <?php
+            if ( is_active_sidebar( 'homepage-mid-banner' ) ) : ?>
+              <?php dynamic_sidebar( 'homepage-mid-banner' ); ?>      
+            <?php endif; ?>
+          </div>
       </div>
-    </div>
-    <div class="col-sm-12 col-lg-8 mrg">
-      <div class="align-self-center courses-slider gy-4">
-        <?php
-        $args_course = array(
-          'post_type' => 'course',
-          'post_status' => 'publish',
-          'posts_per_page' => 3,
-        );
-        $Query_course = new WP_Query( $args_course );
-        if ($Query_course->have_posts()) : while ($Query_course->have_posts()) : $Query_course->the_post();
-          $custom_fields = get_post_custom();
-          $duration = $custom_fields['vibe_duration'][0];
-          $durationParameter = get_post_meta($post->ID,'vibe_course_duration_parameter',true);
-          $session = $custom_fields['vibe_course_sessions'][0];
-          $age_limit = $custom_fields['vibe_course_age_group'][0];
-          $category_array = get_the_terms( $post->ID, 'course-cat');
-          ?>
-          <div class="col-md-12 mrg space" data-aos="zoom-out" data-aos-delay="200">
-            <div class="course-box">
-              <div class="col-xs-2 col-sm-2 col-lg-2 pull-left mrg">
-                <?php 
-                if ( has_post_thumbnail() ) { 
-                  $image_url = get_the_post_thumbnail_url();
-                }
-                ?>
-                <a href="<?php echo get_permalink($post->ID);?>"> <img alt="Celebrity Course" src="<?php echo $image_url; ?>" class="img-fluid"  /></a>
-              </div>
-              <div class="col-xs-10 col-sm-10 col-lg-10 pull-left mrg">
-                <div class="col-sm-12 col-lg-9 pull-left mrg">
-                  <div class="middle-details">
-                    <h6><?php echo $category_array[0]->name; ?></h6>
-                    <h2><?php echo bp_course_title(); ?></h2>
-                    <div class="col-lg-12 share-icon mrg">
-                      <i class="bi bi-share"></i>
-                    </div>
-                    <?php
-                    $excerpt = get_post_field('post_excerpt', $post->ID);
-                    ?>
-                    <?php if ( $excerpt != '' ) {
-                      echo "<p>".wp_trim_words( $excerpt, 20, NULL )."</p>";
-                    }  ?>
-                    <div class="col-lg-7 duration mrg">
-                      <div class="pull-left">
-                        <p>Duration</p>
-                        <?php if($duration == '') { ?>
-                          <h6>--</h6>
-                        <?php } else{ ?>
-                          <h6><?php if($duration != ''){echo $duration; }?><span><?php if($durationParameter != ''){echo ' '.calculate_duration($durationParameter); }?> </span></h6>
-                        <?php }?>
+      <div class="col-sm-12 col-lg-8 mrg">
+        <div class="align-self-center courses-slider gy-4">
+          <?php
+          $args_course = array(
+            'post_type' => 'course',
+            'post_status' => 'publish',
+            'posts_per_page' => 3,
+          );
+          $Query_course = new WP_Query( $args_course );
+          if ($Query_course->have_posts()) : while ($Query_course->have_posts()) : $Query_course->the_post();
+            $custom_fields = get_post_custom();
+            $duration = $custom_fields['vibe_duration'][0];
+            $durationParameter = get_post_meta($post->ID,'vibe_course_duration_parameter',true);
+            $session = $custom_fields['vibe_course_sessions'][0];
+            $age_limit = $custom_fields['vibe_course_age_group'][0];
+            $category_array = get_the_terms( $post->ID, 'course-cat');
+            ?>
+            <div class="col-md-12 mrg space" data-aos="zoom-out" data-aos-delay="200">
+              <div class="course-box">
+                <div class="col-xs-2 col-sm-2 col-lg-2 pull-left mrg">
+                  <?php 
+                  if ( has_post_thumbnail() ) { 
+                    $image_url = get_the_post_thumbnail_url();
+                  }
+                  ?>
+                  <a href="<?php echo get_permalink($post->ID);?>"> <img alt="Celebrity Course" src="<?php echo $image_url; ?>" class="img-fluid"  /></a>
+                </div>
+                <div class="col-xs-10 col-sm-10 col-lg-10 pull-left mrg">
+                  <div class="col-sm-12 col-lg-9 pull-left mrg">
+                    <div class="middle-details">
+                      <h6><?php echo $category_array[0]->name; ?></h6>
+                      <h2><?php echo bp_course_title(); ?></h2>
+                      <div class="col-lg-12 share-icon mrg">
+                        <i class="bi bi-share"></i>
                       </div>
-                      <div class="pull-right">
-                        <p>Age Group</p>
-                        <?php if($age_limit == '') { ?>
-                          <h6>--</h6>
-                        <?php } else{ ?>
-                          <h6><?php echo $age_limit;?><span> yrs</span></h6>
-                        <?php }?>
+                      <?php
+                      $excerpt = get_post_field('post_excerpt', $post->ID);
+                      ?>
+                      <?php if ( $excerpt != '' ) {
+                        echo "<p>".wp_trim_words( $excerpt, 20, NULL )."</p>";
+                      }  ?>
+                      <div class="col-lg-7 duration mrg">
+                        <div class="pull-left">
+                          <p>Duration</p>
+                          <?php if($duration == '') { ?>
+                            <h6>--</h6>
+                          <?php } else{ ?>
+                            <h6><?php if($duration != ''){echo $duration; }?><span><?php if($durationParameter != ''){echo ' '.calculate_duration($durationParameter); }?> </span></h6>
+                          <?php }?>
+                        </div>
+                        <div class="pull-right">
+                          <p>Age Group</p>
+                          <?php if($age_limit == '') { ?>
+                            <h6>--</h6>
+                          <?php } else{ ?>
+                            <h6><?php echo $age_limit;?><span> yrs</span></h6>
+                          <?php }?>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-                <div class="col-sm-12 col-lg-3 pull-left mrg">
-                  <div class="col-lg-12 right-details pull-left mrg">
-                    <div class="share-icon">
-                      <ul>
+                  <div class="col-sm-12 col-lg-3 pull-left mrg">
+                    <div class="col-lg-12 right-details pull-left mrg">
+                      <div class="share-icon">
+                        <ul>
 
-                        <li class="hover_share">
-                          <img alt="share icon" title="share icon" src="<?php echo get_bloginfo('template_url');?>/assets/images/share-icon.svg">
-                          <div class="display_icon">
-                            <h6>Share <span><i class="bi bi-x close-share"></i></span></h6>
-                            <div class="a2a_kit a2a_kit_size_32 a2a_default_style" data-a2a-url="<?php echo get_bloginfo('url')?>/course/<?php echo $post->post_name;?>" data-a2a-title="<?php echo $post->post_title. ' - '.get_bloginfo(); ?>">
-                              <a class="a2a_button_facebook"></a>
-                              <a class="a2a_button_twitter"></a>
-                              <a class="a2a_button_pinterest"></a>
-                              <a class="a2a_button_google_gmail"></a>
-                              <a class="a2a_button_whatsapp"></a>
-                              <a class="a2a_button_telegram"></a>
-                            </div><script async src="https://static.addtoany.com/menu/page.js"></script>
-                          </div>
-                        </li>
-                      </ul>  
-                      <script async src="https://static.addtoany.com/menu/page.js"></script>
+                          <li class="hover_share">
+                            <img alt="share icon" title="share icon" src="<?php echo get_bloginfo('template_url');?>/assets/images/share-icon.svg">
+                            <div class="display_icon">
+                              <h6>Share <span><i class="bi bi-x close-share"></i></span></h6>
+                              <div class="a2a_kit a2a_kit_size_32 a2a_default_style" data-a2a-url="<?php echo get_bloginfo('url')?>/course/<?php echo $post->post_name;?>" data-a2a-title="<?php echo $post->post_title. ' - '.get_bloginfo(); ?>">
+                                <a class="a2a_button_facebook"></a>
+                                <a class="a2a_button_twitter"></a>
+                                <a class="a2a_button_pinterest"></a>
+                                <a class="a2a_button_google_gmail"></a>
+                                <a class="a2a_button_whatsapp"></a>
+                                <a class="a2a_button_telegram"></a>
+                              </div><script async src="https://static.addtoany.com/menu/page.js"></script>
+                            </div>
+                          </li>
+                        </ul>  
+                        <script async src="https://static.addtoany.com/menu/page.js"></script>
+                      </div>
+                      <div class="col-lg-12 course-button mrg">
+                        <h6 ><?php 
+                        the_course_price();
+                        ?>
+                      </h6>
+                      <?php the_course_button(); ?>
+
                     </div>
-                    <div class="col-lg-12 course-button mrg">
-                      <h6 ><?php 
-                      the_course_price();
-                      ?>
-                    </h6>
-                    <?php the_course_button(); ?>
-
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      <?php endwhile; endif; ?>
+        <?php endwhile; endif; ?>
 
-    </div>
-    <a href="<?php echo get_home_url();?>/courses/" class="exlore-link mobile-btn">Discover Courses</a>
+      </div>
+      <div class="col-sm-12 center mrg mobile-add new-add">
+              <img src="https://ht.fortune4.org/wp-content/uploads/2021/04/NT_Sugary-Beverages.jpg">
+      </div>
+      <a href="<?php echo get_home_url();?>/courses/" class="exlore-link mobile-btn">Discover Courses</a>
   </div>
 
 </div> <!-- / row -->

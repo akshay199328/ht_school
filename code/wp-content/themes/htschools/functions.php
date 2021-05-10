de513450ae7a37094c8ed877bed045b6f924b363
sameer <sameer@fortune4.in>
2021-05-10 22:18:15 +0530

    mobile number validation

--- code/wp-content/themes/htschools/functions.php
+++ code/wp-content/themes/htschools/functions.php
@@ -1980,8 +1980,8 @@ if(!function_exists('calculate_duration')){
 
 add_action('woocommerce_checkout_process', 'custom_validate_billing_phone');
     function custom_validate_billing_phone() {
-    $is_correct = preg_match('/^[0-9]{10}$/', $_POST['billing_phone']);
-    if ( $_POST['billing_phone'] && !$is_correct) {
+    $is_correct = preg_match('/^[0-9]{10}$/', $_POST['3']);
+    if ( $_POST['3'] && !$is_correct) {
     wc_add_notice( __( 'The Phone field should be between 10 digits.' ), 'error' );
     }
 }

8cb5e847f9b3e7ea4d53a0bab16a112fecb84d0b
priyankaw-1995 <74304751+priyankaw-1995@users.noreply.github.com>
2021-05-14 10:45:53 +0530

    my course message change

--- code/wp-content/themes/htschools/my-course.php
+++ code/wp-content/themes/htschools/my-course.php
@@ -177,14 +177,14 @@ get_header(vibe_get_header());
                 <?php }else{ ?>
                     <div class="empty_cart_div">
                         <div class="empty_course_image"></div>
-                        <h4>You have not bought any courses till now</h4>
+                        <h4>No Courses here! You're missing out on some cool stuff!</h4>
                         <a href="<?php echo get_home_url();?>/courses/"><button class="empty_btn">Explore All Courses</button></a>
                     </div>
                     
                 <?php }}else{ ?>
                     <div class="empty_cart_div">
                         <div class="empty_course_image"></div>
-                        <h4>You have not bought any courses till now</h4>
+                        <h4>No Courses here! You're missing out on some cool stuff!</h4>
                         <a href="<?php echo get_home_url();?>/courses/"><button class="empty_btn">Explore All Courses</button></a>
                     </div>
                     

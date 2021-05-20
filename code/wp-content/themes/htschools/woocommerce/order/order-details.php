8949cf05b08a74472fcc1ef553bf6f8ea6258116
Noman Sayyed <sayyednoman@gmail.com>
2021-05-20 20:19:32 +0530

    correct order process design

--- code/wp-content/themes/htschools/woocommerce/order/order-details.php
+++ code/wp-content/themes/htschools/woocommerce/order/order-details.php
@@ -44,7 +44,8 @@ if ( $show_downloads ) {
 
 	<h2 class="woocommerce-order-details__title"><?php esc_html_e( 'Order details', 'woocommerce' ); ?></h2>
 
-	<table class="woocommerce-table woocommerce-table--order-details shop_table order_details">
+	<div class="table-copy">
+		<table class="woocommerce-table woocommerce-table--order-details shop_table order_details">
 
 		<thead>
 			<tr>
@@ -96,6 +97,7 @@ if ( $show_downloads ) {
 			<?php endif; ?>
 		</tfoot>
 	</table>
+	</div>
 
 	<?php do_action( 'woocommerce_order_details_after_order_table', $order ); ?>
 </section>

5e317dd2b83a7164f40e1c1684762b3f3b4615b3
kedargandre-fortune4 <kedar.g@fortune4.in>
2021-05-28 19:31:41 +0530

    Update Google ads logic

--- code/wp-content/themes/htschools/google-ads.php
+++ code/wp-content/themes/htschools/google-ads.php
@@ -195,6 +195,7 @@
 
 		ads.map((value, index) => {
 			if(jQuery("#" + value.divid).length > 0 && jQuery("#" + value.divid).is(":visible")) {
+				jQuery("#" + value.divid).before('<span class="adwork_text">Advertisement</span>');
 				googletag.defineSlot(value.adcode, value.adsize, value.divid).addService(googletag.pubads());
 			}
 		});

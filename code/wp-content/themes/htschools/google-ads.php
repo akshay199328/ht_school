7eacc1385e80071918828c9dc3548b291af81715
kedargandre-fortune4 <kedar.g@fortune4.in>
2021-05-28 19:02:42 +0530

    Google code logic updated

---
+++ code/wp-content/themes/htschools/google-ads.php
@@ -0,0 +1,206 @@
+<script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"></script>
+<script>
+	var isMobileDevice = (window.screen.availWidth <= 640) ? true : false;
+
+	var desktopAdsDetails = [
+		{
+			"adcode": "/1055314/HT_School_Desktop_Home/HT_School_Desk_HP_Footer_Billboard",
+			"adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+			"divid": "div-gpt-ad-1619596100543-1",
+		},
+		{
+			"adcode": "/1055314/HT_School_Desktop_Section/HT_School_Desk_Section_MID_A_Billboard",
+			"adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+			"divid": "div-gpt-ad-1619596100543-2",
+		},
+		{
+			"adcode": "/1055314/HT_School_Desktop_Story/HT_School_Desk_Story_MID_A_Billboard",
+			"adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+			"divid": "div-gpt-ad-1619596100543-4",
+		},
+		{
+			"adcode": "/1055314/HT_School_Desktop_Story/HT_School_Desk_Story_MID_A_Billboard",
+			"adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+			"divid": "div-gpt-ad-1619596100543-5",
+		},
+		{
+			"adcode": "/1055314/HT_School_Desktop_Story/HT_School_Desk_Story_Footer_Billboard",
+			"adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+			"divid": "div-gpt-ad-1619596100543-6",
+		},
+		{
+			"adcode": "/1055314/HT_School_Desktop_Story/HT_School_Desk_Story_Footer_Billboard",
+			"adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+			"divid": "div-gpt-ad-1619596100543-12",
+		},
+		{
+			"adcode": "/1055314/HT_School_Desktop_Story/HT_School_Desk_Story_Footer_Billboard",
+			"adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+			"divid": "div-gpt-ad-1619596100543-13",
+		},
+		{
+			"adcode": "/1055314/HT_School_Desktop_Story/HT_School_Desk_Story_Footer_Billboard",
+			"adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+			"divid": "div-gpt-ad-1619596100543-14",
+		},
+		{
+			"adcode": "/1055314/HT_School_Desktop_Story/HT_School_Desk_Story_Footer_Billboard",
+			"adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+			"divid": "div-gpt-ad-1619596100543-15",
+		},
+		{
+			"adcode": "/1055314/HT_School_Desktop_Story/HT_School_Desk_Story_Footer_Billboard",
+			"adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+			"divid": "div-gpt-ad-1619596100543-16",
+		},
+		{
+			"adcode": "/1055314/HT_School_Desktop_Story/HT_School_Desk_Story_Footer_Billboard",
+			"adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+			"divid": "div-gpt-ad-1619596100543-17",
+		},
+		{
+			"adcode": "/1055314/HT_School_Desktop_Story/HT_School_Desk_Story_Footer_Billboard",
+			"adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+			"divid": "div-gpt-ad-1619596100543-18",
+		},
+		{
+			"adcode": "/1055314/HT_School_Desktop_Home/HT_School_Desk_HP_MID_A_Mrec",
+			"adsize": [[300,250]],
+			"divid": "div-gpt-ad-1619596100543-0",
+		},
+		{
+			"adcode": "/1055314/HT_School_Desktop_Section/HT_School_Desk_Section_MID_A_Mrec",
+			"adsize": [[300,250]],
+			"divid": "div-gpt-ad-1619596100543-7",
+		},
+		{
+			"adcode": "/1055314/HT_School_Desktop_Story/HT_School_Desk_Story_MID_A_Mrec",
+			"adsize": [[300,250]],
+			"divid": "div-gpt-ad-1619596100543-8",
+		},
+		{
+			"adcode": "/1055314/HT_School_Desktop_Section/HT_School_Desk_Section_MID_A_Mrec",
+			"adsize": [[300,250]],
+			"divid": "div-gpt-ad-1619596100543-11",
+		},
+		{
+			"adcode": "/1055314/HT_School_Desktop_Section/HT_School_Desk_Section_MID_A_Mrec",
+			"adsize": [[300,250]],
+			"divid": "div-gpt-ad-1619596100543-20",
+		},
+		{
+			"adcode": "/1055314/HT_School_Desktop_Section/HT_School_Desk_Section_MID_A_Mrec",
+			"adsize": [[300,250]],
+			"divid": "div-gpt-ad-1619596100543-3",
+		},
+	];
+
+	var mobileAdsDetails = [
+		{
+			"adcode": "/1055314/HT_School_WAP_Home/HT_School_Desk_HP_MID_Footer_Mrec",
+			"adsize": [[300,250], [250,250], [300,600], [336,280], [1,1]],
+			"divid": "div-gpt-ad-1619596100543-1",
+		},
+		{
+			"adcode": "/1055314/HT_School_Desktop_Section/HT_School_Desk_Section_MID_A_Billboard",
+			"adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+			"divid": "div-gpt-ad-1619596100543-2",
+		},
+		{
+			"adcode": "/1055314/HT_School_Desktop_Story/HT_School_Desk_Story_MID_A_Billboard",
+			"adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+			"divid": "div-gpt-ad-1619596100543-4",
+		},
+		{
+			"adcode": "/1055314/HT_School_Desktop_Story/HT_School_Desk_Story_MID_A_Billboard",
+			"adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+			"divid": "div-gpt-ad-1619596100543-5",
+		},
+		{
+			"adcode": "/1055314/HT_School_WAP_Story/HT_School_Desk_Story_MID_Footer_Mrec",
+			"adsize": [[300,250], [250,250], [300,600], [336,280], [1,1]],
+			"divid": "div-gpt-ad-1619596100543-6",
+		},
+		{
+			"adcode": "/1055314/HT_School_WAP_Story/HT_School_Desk_Story_MID_Footer_Mrec",
+			"adsize": [[300,250], [250,250], [300,600], [336,280], [1,1]],
+			"divid": "div-gpt-ad-1619596100543-12",
+		},
+		{
+			"adcode": "/1055314/HT_School_WAP_Story/HT_School_Desk_Story_MID_Footer_Mrec",
+			"adsize": [[300,250], [250,250], [300,600], [336,280], [1,1]],
+			"divid": "div-gpt-ad-1619596100543-13",
+		},
+		{
+			"adcode": "/1055314/HT_School_WAP_Story/HT_School_Desk_Story_MID_Footer_Mrec",
+			"adsize": [[300,250], [250,250], [300,600], [336,280], [1,1]],
+			"divid": "div-gpt-ad-1619596100543-14",
+		},
+		{
+			"adcode": "/1055314/HT_School_WAP_Story/HT_School_Desk_Story_MID_Footer_Mrec",
+			"adsize": [[300,250], [250,250], [300,600], [336,280], [1,1]],
+			"divid": "div-gpt-ad-1619596100543-15",
+		},
+		{
+			"adcode": "/1055314/HT_School_WAP_Story/HT_School_Desk_Story_MID_Footer_Mrec",
+			"adsize": [[300,250], [250,250], [300,600], [336,280], [1,1]],
+			"divid": "div-gpt-ad-1619596100543-16",
+		},
+		{
+			"adcode": "/1055314/HT_School_WAP_Story/HT_School_Desk_Story_MID_Footer_Mrec",
+			"adsize": [[300,250], [250,250], [300,600], [336,280], [1,1]],
+			"divid": "div-gpt-ad-1619596100543-17",
+		},
+		{
+			"adcode": "/1055314/HT_School_WAP_Story/HT_School_Desk_Story_MID_Footer_Mrec",
+			"adsize": [[300,250], [250,250], [300,600], [336,280], [1,1]],
+			"divid": "div-gpt-ad-1619596100543-18",
+		},
+		{
+			"adcode": "/1055314/HT_School_Desktop_Home/HT_School_Desk_HP_MID_A_Mrec",
+			"adsize": [[300,250]],
+			"divid": "div-gpt-ad-1619596100543-0",
+		},
+		{
+			"adcode": "/1055314/HT_School_Desktop_Section/HT_School_Desk_Section_MID_A_Mrec",
+			"adsize": [[300,250]],
+			"divid": "div-gpt-ad-1619596100543-7",
+		},
+		{
+			"adcode": "/1055314/HT_School_WAP_Story/HT_School_Desk_Story_MID_B_Mrec",
+			"adsize": [[300,250], [250,250], [300,600], [336,280]],
+			"divid": "div-gpt-ad-1619596100543-8",
+		},
+		{
+			"adcode": "/1055314/HT_School_Desktop_Section/HT_School_Desk_Section_MID_A_Mrec",
+			"adsize": [[300,250]],
+			"divid": "div-gpt-ad-1619596100543-11",
+		},
+		{
+			"adcode": "/1055314/HT_School_Desktop_Section/HT_School_Desk_Section_MID_A_Mrec",
+			"adsize": [[300,250]],
+			"divid": "div-gpt-ad-1619596100543-20",
+		},
+		{
+			"adcode": "/1055314/HT_School_Desktop_Section/HT_School_Desk_Section_MID_A_Mrec",
+			"adsize": [[300,250]],
+			"divid": "div-gpt-ad-1619596100543-3",
+		},
+	];
+
+	window.googletag = window.googletag || {cmd: []};
+	googletag.cmd.push(function() {
+
+		let ads = isMobileDevice ? mobileAdsDetails : desktopAdsDetails;
+
+		ads.map((value, index) => {
+			if(jQuery("#" + value.divid).length > 0 && jQuery("#" + value.divid).is(":visible")) {
+				googletag.defineSlot(value.adcode, value.adsize, value.divid).addService(googletag.pubads());
+			}
+		});
+
+		googletag.pubads().enableSingleRequest();
+		googletag.pubads().collapseEmptyDivs();
+		googletag.enableServices();
+	});
+</script>
--- code/wp-content/themes/htschools/header-bk-gapi.php
+++ code/wp-content/themes/htschools/header-bk-gapi.php
@@ -21,17 +21,211 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-MVTH49L');</script>
 <!-- End Google Tag Manager -->
+<script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"></script>
+<script>
 
-<?php global $wp;
+  var isMobileDevice = (window.screen.availWidth <= 640) ? true : false;
 
-$currentSlug = add_query_arg( array(), $wp->request );
-$currentSlug = $currentSlug ? $currentSlug : "home";
-$currentSlug = explode('/', $currentSlug)[0];
+  var desktopAdsDetails = [
+    {
+      "adcode": "/1055314/HT_School_Desktop_Home/HT_School_Desk_HP_Footer_Billboard",
+      "adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+      "divid": "div-gpt-ad-1619596100543-1",
+    },
+    {
+      "adcode": "/1055314/HT_School_Desktop_Section/HT_School_Desk_Section_MID_A_Billboard",
+      "adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+      "divid": "div-gpt-ad-1619596100543-2",
+    },
+    {
+      "adcode": "/1055314/HT_School_Desktop_Story/HT_School_Desk_Story_MID_A_Billboard",
+      "adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+      "divid": "div-gpt-ad-1619596100543-4",
+    },
+    {
+      "adcode": "/1055314/HT_School_Desktop_Story/HT_School_Desk_Story_MID_A_Billboard",
+      "adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+      "divid": "div-gpt-ad-1619596100543-5",
+    },
+    {
+      "adcode": "/1055314/HT_School_Desktop_Story/HT_School_Desk_Story_Footer_Billboard",
+      "adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+      "divid": "div-gpt-ad-1619596100543-6",
+    },
+    {
+      "adcode": "/1055314/HT_School_Desktop_Story/HT_School_Desk_Story_Footer_Billboard",
+      "adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+      "divid": "div-gpt-ad-1619596100543-12",
+    },
+    {
+      "adcode": "/1055314/HT_School_Desktop_Story/HT_School_Desk_Story_Footer_Billboard",
+      "adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+      "divid": "div-gpt-ad-1619596100543-13",
+    },
+    {
+      "adcode": "/1055314/HT_School_Desktop_Story/HT_School_Desk_Story_Footer_Billboard",
+      "adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+      "divid": "div-gpt-ad-1619596100543-14",
+    },
+    {
+      "adcode": "/1055314/HT_School_Desktop_Story/HT_School_Desk_Story_Footer_Billboard",
+      "adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+      "divid": "div-gpt-ad-1619596100543-15",
+    },
+    {
+      "adcode": "/1055314/HT_School_Desktop_Story/HT_School_Desk_Story_Footer_Billboard",
+      "adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+      "divid": "div-gpt-ad-1619596100543-16",
+    },
+    {
+      "adcode": "/1055314/HT_School_Desktop_Story/HT_School_Desk_Story_Footer_Billboard",
+      "adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+      "divid": "div-gpt-ad-1619596100543-17",
+    },
+    {
+      "adcode": "/1055314/HT_School_Desktop_Story/HT_School_Desk_Story_Footer_Billboard",
+      "adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+      "divid": "div-gpt-ad-1619596100543-18",
+    },
+    {
+      "adcode": "/1055314/HT_School_Desktop_Home/HT_School_Desk_HP_MID_A_Mrec",
+      "adsize": [[300,250]],
+      "divid": "div-gpt-ad-1619596100543-0",
+    },
+    {
+      "adcode": "/1055314/HT_School_Desktop_Section/HT_School_Desk_Section_MID_A_Mrec",
+      "adsize": [[300,250]],
+      "divid": "div-gpt-ad-1619596100543-7",
+    },
+    {
+      "adcode": "/1055314/HT_School_Desktop_Story/HT_School_Desk_Story_MID_A_Mrec",
+      "adsize": [[300,250]],
+      "divid": "div-gpt-ad-1619596100543-8",
+    },
+    {
+      "adcode": "/1055314/HT_School_Desktop_Section/HT_School_Desk_Section_MID_A_Mrec",
+      "adsize": [[300,250]],
+      "divid": "div-gpt-ad-1619596100543-11",
+    },
+    {
+      "adcode": "/1055314/HT_School_Desktop_Section/HT_School_Desk_Section_MID_A_Mrec",
+      "adsize": [[300,250]],
+      "divid": "div-gpt-ad-1619596100543-20",
+    },
+    {
+      "adcode": "/1055314/HT_School_Desktop_Section/HT_School_Desk_Section_MID_A_Mrec",
+      "adsize": [[300,250]],
+      "divid": "div-gpt-ad-1619596100543-3",
+    },
+  ];
 
-$allowAdsPageList = array("home", "courses", "course", "editorsdesk", "my-courses");
-if(in_array($currentSlug, $allowAdsPageList) || strlen($currentSlug)) require_once('google-ads.php');
-?>
+  var mobileAdsDetails = [
+    {
+      "adcode": "/1055314/HT_School_WAP_Home/HT_School_Desk_HP_MID_Footer_Mrec",
+      "adsize": [[300,250], [250,250], [300,600], [336,280], [1,1]],
+      "divid": "div-gpt-ad-1619596100543-1",
+    },
+    {
+      "adcode": "/1055314/HT_School_Desktop_Section/HT_School_Desk_Section_MID_A_Billboard",
+      "adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+      "divid": "div-gpt-ad-1619596100543-2",
+    },
+    {
+      "adcode": "/1055314/HT_School_Desktop_Story/HT_School_Desk_Story_MID_A_Billboard",
+      "adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+      "divid": "div-gpt-ad-1619596100543-4",
+    },
+    {
+      "adcode": "/1055314/HT_School_Desktop_Story/HT_School_Desk_Story_MID_A_Billboard",
+      "adsize": [[728,90], [970,90], [970,250], [1290,250],[980,90], [970,90]],
+      "divid": "div-gpt-ad-1619596100543-5",
+    },
+    {
+      "adcode": "/1055314/HT_School_WAP_Story/HT_School_Desk_Story_MID_Footer_Mrec",
+      "adsize": [[300,250], [250,250], [300,600], [336,280], [1,1]],
+      "divid": "div-gpt-ad-1619596100543-6",
+    },
+    {
+      "adcode": "/1055314/HT_School_WAP_Story/HT_School_Desk_Story_MID_Footer_Mrec",
+      "adsize": [[300,250], [250,250], [300,600], [336,280], [1,1]],
+      "divid": "div-gpt-ad-1619596100543-12",
+    },
+    {
+      "adcode": "/1055314/HT_School_WAP_Story/HT_School_Desk_Story_MID_Footer_Mrec",
+      "adsize": [[300,250], [250,250], [300,600], [336,280], [1,1]],
+      "divid": "div-gpt-ad-1619596100543-13",
+    },
+    {
+      "adcode": "/1055314/HT_School_WAP_Story/HT_School_Desk_Story_MID_Footer_Mrec",
+      "adsize": [[300,250], [250,250], [300,600], [336,280], [1,1]],
+      "divid": "div-gpt-ad-1619596100543-14",
+    },
+    {
+      "adcode": "/1055314/HT_School_WAP_Story/HT_School_Desk_Story_MID_Footer_Mrec",
+      "adsize": [[300,250], [250,250], [300,600], [336,280], [1,1]],
+      "divid": "div-gpt-ad-1619596100543-15",
+    },
+    {
+      "adcode": "/1055314/HT_School_WAP_Story/HT_School_Desk_Story_MID_Footer_Mrec",
+      "adsize": [[300,250], [250,250], [300,600], [336,280], [1,1]],
+      "divid": "div-gpt-ad-1619596100543-16",
+    },
+    {
+      "adcode": "/1055314/HT_School_WAP_Story/HT_School_Desk_Story_MID_Footer_Mrec",
+      "adsize": [[300,250], [250,250], [300,600], [336,280], [1,1]],
+      "divid": "div-gpt-ad-1619596100543-17",
+    },
+    {
+      "adcode": "/1055314/HT_School_WAP_Story/HT_School_Desk_Story_MID_Footer_Mrec",
+      "adsize": [[300,250], [250,250], [300,600], [336,280], [1,1]],
+      "divid": "div-gpt-ad-1619596100543-18",
+    },
+    {
+      "adcode": "/1055314/HT_School_Desktop_Home/HT_School_Desk_HP_MID_A_Mrec",
+      "adsize": [[300,250]],
+      "divid": "div-gpt-ad-1619596100543-0",
+    },
+    {
+      "adcode": "/1055314/HT_School_Desktop_Section/HT_School_Desk_Section_MID_A_Mrec",
+      "adsize": [[300,250]],
+      "divid": "div-gpt-ad-1619596100543-7",
+    },
+    {
+      "adcode": "/1055314/HT_School_WAP_Story/HT_School_Desk_Story_MID_B_Mrec",
+      "adsize": [[300,250], [250,250], [300,600], [336,280]],
+      "divid": "div-gpt-ad-1619596100543-8",
+    },
+    {
+      "adcode": "/1055314/HT_School_Desktop_Section/HT_School_Desk_Section_MID_A_Mrec",
+      "adsize": [[300,250]],
+      "divid": "div-gpt-ad-1619596100543-11",
+    },
+    {
+      "adcode": "/1055314/HT_School_Desktop_Section/HT_School_Desk_Section_MID_A_Mrec",
+      "adsize": [[300,250]],
+      "divid": "div-gpt-ad-1619596100543-20",
+    },
+    {
+      "adcode": "/1055314/HT_School_Desktop_Section/HT_School_Desk_Section_MID_A_Mrec",
+      "adsize": [[300,250]],
+      "divid": "div-gpt-ad-1619596100543-3",
+    },
+  ];
 
+  window.googletag = window.googletag || {cmd: []};
+  googletag.cmd.push(function() {
+
+    let ads = isMobileDevice ? mobileAdsDetails : desktopAdsDetails;
+
+    ads.map((value, index) => {
+      googletag.defineSlot(value.adcode, value.adsize, value.divid).addService(googletag.pubads());
+    });
+
+    googletag.pubads().enableSingleRequest();
+    googletag.pubads().collapseEmptyDivs();
+    googletag.enableServices();
+  });
+</script>
 </head>
 <body <?php body_class(); ?>>
 <div id="global" class="global">

ab2f9a83b5cdb4135bd5824eb429bea37ea148c5
priyankaw-1995 <74304751+priyankaw-1995@users.noreply.github.com>
2021-05-29 12:48:21 +0530

    bootstrap cdn

--- code/wp-content/themes/htschools/functions.php
+++ code/wp-content/themes/htschools/functions.php
@@ -244,7 +244,7 @@ add_action( 'widgets_init', 'wp_bootstrap_starter_widgets_init' );
     wp_enqueue_style( 'wplms-customizer-css2', get_template_directory_uri(). '/style.css' );
     wp_enqueue_style( 'wplms-custom', get_template_directory_uri(). '/custom.css' );
     wp_enqueue_style( 'wplms-bootstrap-icons', get_template_directory_uri(). '/assets/vendor/bootstrap-icons/bootstrap-icons.css' );
-    wp_enqueue_style( 'wplms-bootstrap', get_template_directory_uri(). '/assets/vendor/bootstrap/css/bootstrap.css' );
+    wp_enqueue_style( 'wplms-bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' );
     wp_enqueue_style( 'wplms-navigation', get_template_directory_uri(). '/navigation.css' );
     wp_enqueue_style( 'wplms-responsive', get_template_directory_uri(). '/assets/css/responsive.css');
     wp_enqueue_script( 'wplms-jquery', get_template_directory_uri(). '/assets/js/jquery.min.js', '', '', true );

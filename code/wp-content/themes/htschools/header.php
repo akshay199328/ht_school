89f1eee1143974d67d4005059e03ee4c441af428
sameer <sameer@fortune4.in>
2021-06-14 09:56:48 +0530

    gtm of staging

--- code/wp-content/themes/htschools/header.php
+++ code/wp-content/themes/htschools/header.php
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MVTH49L');</script>
+})(window,document,'script','dataLayer','GTM-MXXRRC3');</script>
 <!-- End Google Tag Manager -->
 
 <?php global $wp;

f3f4e239347919b64532fd3c3a11d93c334e236b
bhavna <bhavna.langi@fortune4.in>
2021-07-16 13:32:15 +0530

    Dashboard changes

--- HTML/codeathon/dashboard.php
+++ HTML/codeathon/dashboard.php
@@ -341,7 +341,7 @@
                                     <h6>Earn points</h6>
                                 </div>
                                 <div class="board-button">
-                                    <button type="button">REFERRALS</button>
+                                    <button type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop">REFERRALS</button>
                                     <button type="button">UPLOAD PROJECT</button>
                                 </div>
                             </div>
@@ -599,4 +599,23 @@
             </div>
             </div>
     </section>
+
+    <!-- Modal Refer Friends-->
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Understood</button>
+          </div>
+        </div>
+      </div>
+    </div>
 <?php include("includes/footer.php"); ?>

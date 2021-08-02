5a9c046141cbceffde8b0c6cb5e37fdff3cae5b9
bhavna <bhavna.langi@fortune4.in>
2021-08-02 14:30:49 +0530

    UI Changes

--- schools/application/config.php
+++ schools/application/config.php
@@ -2,6 +2,6 @@
 $db_host	    = 'localhost';
 $db_user        = 'root';
 $db_password	= '';
-$db_name	    = 'schools_ht';
-define('APP_URL', 'http://localhost/Htschools-git/schools');
+$db_name	    = 'htschools';
+define('APP_URL', 'http://localhost/Htschools/schools');
 $_app_stage = 'Live'; // You can set this variable Live to Dev to enable ibilling Debug

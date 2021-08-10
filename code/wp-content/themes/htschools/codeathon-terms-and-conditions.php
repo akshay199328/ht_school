c02cc0e36442014dd0c8dffdeea64d63ccf77eac
Noman Sayyed <sayyednoman@gmail.com>
2021-08-10 12:20:25 +0530

    school background

--- code/wp-content/themes/htschools/codeathon-terms-and-conditions.php
+++ code/wp-content/themes/htschools/codeathon-terms-and-conditions.php
@@ -5,7 +5,143 @@
 
 if ( !defined( 'ABSPATH' ) ) exit;
 
-include("includes/lead.php");
-?>
+include("includes/lead.php"); ?>
+
+<div class="section-wrapper terms-conditions">
+	<div class="section-copy">
+		<h2 class="section-title">Tearms and Conditions</h2>
+		<ol>
+			<li>These Terms and Conditions ("T&C"), as amended from time to time are the complete and exclusive statements of understanding between HT Media Limited and its affiliate(s) ("HTML") & the Participant/Users (through Parent/Legal guardian/School) and shall enroll for the course on HT Codeathon (hereinafter referred to as the "Program") and to participate in HT Jr. Code <code>&lt;a&gt;</code> thon Qualifier and Finale (hereinafter referred to as "Olympiad"). It supersedes all other prior understanding, whether oral or written. All representation or other communications between the HTML & the Participant amounts to acceptance of these terms & conditions.</li>
+			<li>HTML is offering a premium course in Coding ("Program") and a Coding Olympiad ("Olympiad") for the students of VI to IX Grade. The User/Participants may enroll either directly or through the respective schools.</li>
+			<li><strong>Age of the Participant:</strong> A Participant must be within 16 years of age in order to be eligible for competing in the Olympiad. HTML reserves the right to disqualify Participants for any misrepresentation or otherwise</li>
+			<li>HTML shall provide an online platform for enrolling into the Program and participating in the Olympiad to the Users/Participants. All registration and payment procedures shall be in accordance with the terms and conditions herein, as may be amended from time to time and/or any other communication as shared by HTML from time to time. </li>
+			<li>By enrolling into the Program and/or participating in the Olympiad, a Participant unconditionally and irrevocably accepts and agrees to be bound by all these terms and conditions as stated herein. This Olympiad is subject to the laws of India, and is not offered outside of India. </li>
+			<li>HTML shall not be responsible for any Participant enrolling into the Program and/or entering the Olympiad unlawfully or otherwise in breach of any applicable laws.</li>
+			<li>HTML reserves the right, in its sole and absolute discretion, to terminate or disqualify any Participant’s enrollment/participation to the Program and/or the Olympiad or reclaim the prize at any time if it is the opinion of HTML that there has been a breach of any of the Terms by such Participant (whether discovered during or after the end of the Olympiad).</li>
+			<li>All the contents and material of the Program and the Olympiad is an Intellectual Property of HTML and the User/Participant shall have no right over the same.</li>
+			<li>Any commercial use of the content of the Program and the Olympiad is strictly prohibited and shall be punishable under applicable laws.</li>
+			<li>
+				<p>The Registration Process is as follows</p>
+				<strong>Registration Process:</strong>
+				<p><strong>Registrations may be done by Individual Users or Bulk registrations can be done through schools.</strong></p>
+				<p><strong>Individual Registration</strong></p>
+				<ul>
+					<li>User shall visit <a href="https://www.htcodeathon.com/" target="_blank">www.htcodeathon.com</a>  or <a href="https://htschool.hindustantimes.com/" target="_blank">https://htschool.hindustantimes.com/</a></li>
+					<li>User shall create an account either for himself/herself or for the prospective Participant.</li>
+					<li>User will sign up with personal details such as Name, Email id, Contact Number, School Details and Photo or any other details as may be required. Digital copy of the School ID card as a proof of identity is mandatory.</li>
+					<li>Post successful signup, the User/Participant will be directed to the payment page where payment can be made using debit/credit card, net banking, UPI, digital wallets etc.</li>
+					<li>If a parent wishes to register more than one - user, the Parent can use the same email-id and contact number for other user registration. Based on a Participant’s Grade /Class the respective Program will be assigned automatically.</li>
+					<li>The Participant will be earning points at every step of the registration process and payment.</li>
+					<li>Once the payment has been realized by HTML, access to the program shall be granted to Participant.</li>
+					<li>The access to the Program through the Account to the Participant shall be granted for a period of 06 months from the date of registration. </li>
+				</ul>
+				<p><strong>Bulk Registrations</strong></p>
+				<p>Bulk Registration can be done for schools opting for the same through the following process:</p>
+				<ul>
+					<li><p>The schools will share the following details in an excel file:</p>
+						<ul>
+							<li>Participant’s Name</li>
+							<li>Participant’s DOB</li>
+							<li>Parent's email</li>
+							<li>Parent's phone number</li>
+							<li>School Name</li>
+							<li>Standard/Grade</li>
+							<li>Section</li>
+						</ul>
+					</li>
+					<li><p>The school may opt for following modes of payment.</p>
+						<ul>
+							<li>Online payment via link</li>
+							<li>Demand Draft in favor of HT Media Limited</li>
+							<li>Bank Transfer through NEFT/RTGS</li>
+						</ul>
+					</li>
+					<li>The invoice will be shared to the school with the total amount along with the number of Participants</li>
+					<li>Participants will login using  the registered email-id</li>
+					<li>Every time the student wishes to Login, OTP will be sent to the registered email-id which can be used to login.</li>
+					<li>Based on a Participant’s Grade /Class the respective Program will be assigned automatically.</li>
+					<li>The Participant will be earning points at every step of the registration process and payment.</li>
+				</ul>
+				<strong>NOTE</strong>
+				<ul>
+					<li>Registration Fee for each Participant for the entire Program shall be INR 499/- + applicable taxes.</li>
+					<li>HTML reserves the right to disqualify Participants for misrepresenting their school/class details.</li>
+				</ul>
+			</li>
+			<li>
+				<p><strong>Participant Dashboard</strong></p>
+				<ul>
+					<li>After registration, the Participants will be taken to their respective dashboards where they can find all the various details regarding their respective Program modules, qualifiers and finale.</li>
+					<li>
+						<p>Details about the following can be referred to on the dashboard:</p>
+						<ul>
+							<li>Details about the scoring and the point system.</li>
+							<li>Details about the course videos, chapter quizzes, mock qualifiers and qualifiers.</li>
+							<li>Details about qualification and the scoring process for the finale.</li>
+							<li>Program Content</li>
+							<li>Leadership Board</li>
+						</ul>
+					</li>
+					<li>A Chat support is also available for the Users/Participants which will help them solve questions/logics. The chat support shall revert to the queries raised by student with 24 working hours.</li>
+				</ul>
+			</li>
+			<li>
+				<p><strong>Details of the Program are as follows:</strong><br>
+					<strong>Program Details:</strong></p>
+				<ul>
+					<li>One of India’s Biggest Coding Olympiad</li>
+					<li>Training Course focused on learning modules followed by Online Qualifiers and Online/On ground Finale as per the discretion of HTML. 20-24hour self-paced online course on coding via online video’s.</li>
+					<li>Multiple activities to test Participant's progress viz. Quiz, Assessment, and final project.</li>
+					<li>Customized course for Participants from 4th and 5th , 6th and 7th Grade and 8th and 9th Grade. </li>
+					<li>Category 1: App Development</li>
+					<li>Category 2: Website development using HTML, CSS & Java Script and Bootsrap</li>
+					<li>Category 3: Game development with Python , Pyiglet</li>
+				</ul>
+				<p><strong>Program Content</strong></p>
+				<ul>
+					<li><p>The Program content will comprise of the following elements:</p>
+						<ul>
+							<li>Multiple Chapters</li>
+							<li>Video Content: These will be self-paced learning modules (Video and Text contents)</li>
+							<li>Video Quizzes: These will appear once the Participant completes a video </li>
+							<li>Chapter Quizzes: These will appear at the end of every chapter</li>
+							<li>Course Quiz: This will appear at the end of the course</li>
+						</ul>
+					</li>
+					<li>All videos, chapter quizzes are unsurpassable</li>
+					<li>Participants need to complete one video and quiz before he goes to the next one </li>
+					<li>Completion of all of the videos and video quizzes is necessary for eligibility for the Code<code>&lt;a&gt;</code>thon Qualifiers.</li>
+					<li>There are points awarded on completion of every Video and Chapter Quiz.</li>
+					<li>The Participant will be ranked on the acquired points during the course of the program.</li>
+				</ul>
+				<p><strong>Olympiad Progression</strong><br>
+					<strong>Qualifiers</strong></p>
+				<ul>
+					<li>Qualifier will take place in the month of November/ December 2021. Exact dates will be communicated eventually.</li>
+					<li>To reach the Qualifiers a Participant needs to complete all the chapter and quizzes, including all videos and video quizzes.</li>
+					<li>The Participant needs to pass the chapter quizzes with a minimum of 60% or above. The Participants get two chances to earn points on completing the chapter quiz. No points can be earned for completing the quiz on a third or beyond (s) attempt. </li>
+					<li>Qualifiers will be held online and there will be one attempt available to the Participants.</li>
+					<li>The Outcome of the Qualifiers will be published on the Participant’s Dashboard and a separate email shall be sent to the Participants. A digital certificate of participation shall be issued to each Participant.  </li>
+				</ul>
+				<p><strong>Finale</strong><br>
+					<strong>Qualifiers</strong></p>
+				<ul>
+					<li>Finale will take place on in the month of December 2021.</li>
+					<li>The Finale will take place at Delhi and Mumbai. The venue shall be communicated to the top 103 (including 3 Wild card Entries) Qualifiers per zone from each batch of 4th and 5th , 6th and 7th and 8th and 9th eventually.</li>
+					<li>
+						<p>The Finale will take place in Delhi and Mumbai. HTML will segregate the Participants into nearest zone based on their current residence.</p>
+						<p>North Zone : Assam, Bihar, , Haryana, Himachal Pradesh, Jammu and Kashmir, Jharkhand, Manipur, Meghalaya, Mizoram, Nagaland, Arunachal Pradesh, ,New Delhi, , Punjab, Rajasthan, Tripura, Uttar Pradesh, Uttarakhand, West Bengal</p>
+						<p>South Zone : Andaman and Nicobar Islands, Andhra Pradesh, Dadra and Nagar Haveli, Goa, Gujarat, Chhattisgarh , Odisha Karnataka, Kerala, Madhya Pradesh, Maharashtra, Puducherry, Tamil Nadu, Telangana, Lakshwadeep,</p>
+					</li>
+					<li>HT in its sole discretion may introduce a maximum of (03) Participant(s) directly in the Finale (without the mandate of qualifying the Qualifier) as wild card entries. However, the same shall be in addition to the 100 participants qualified for finale through the Qualifier. Details of such wild card entries shall be disclosed only at the time to introducing such wild card entries to HT Codeathon Finale.</li>
+					<li>Finale may take place online and Participants will be notified well in advance. Points earned/acquired during the entire Program will be taken into account for a Participant to qualify for the Code<code>&lt;a&gt;</code>thon Finale.</li>
+					<li>The finalists will be chosen based on the overall performance and points collected till the end of Qualifiers.</li>
+					<li>The Outcome of the Finale will be published on the website and emails will be sent to the Participants.</li>
+				</ul>
+			</li>
+			<li>Outcome of the Olympiad shall be final and binding any challenge to the same shall be not entertained. </li>
+		</ol>
+	</div>
+</div>
 
 <?php include("includes/footer.php"); ?>

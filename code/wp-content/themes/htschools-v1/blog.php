1acf665428eb73887ac21b0946032889589195bc
bhavnaFortune4 <bhavna.langi@fortune4.in>
2021-08-23 17:24:25 +0530

    UI Changes

--- code/wp-content/themes/htschools-v1/blog.php
+++ code/wp-content/themes/htschools-v1/blog.php
@@ -37,237 +37,143 @@ $locations = get_nav_menu_locations();
 $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
 $menuitems = wp_get_nav_menu_items( $menu->term_id, array( 'order' => 'DESC' ) );
 ?>
-
-
-<!-- ======= Featured ======= -->
-
-<section id="All" data-anchor="All" class="latest-news grey-background">
-  <div class="container">
-    <div class="">
-      <div class="">
-     <div class="featured_tablist mrg">
-        <ul class="tablist left_tab">
-      <?php
-      ?>
-        <li class="active news-li" data-scroll="All" id="All"><a href="#" data-id="All">Latest</a></li>
-      <?php
-      foreach ($menuitems as $menu) {
-        ?>
-        <li id="<?php echo $menu->ID; ?>" class="news-li" data-scroll="<?php echo $menu->ID; ?>"><a href="<?php echo $menu->url; ?> " data-id="<?php echo $menu->ID; ?>"><?php echo $menu->title; ?></a></li>
-        <?php
-      }
-      ?>
-      </ul>
-    </div>
-    <!-- <div class="featured_headeing">
-      <h1>Featured News</h1>
-    </div> -->
-    <?php
-    $args = array(
-      'post_type' => 'post',
-      'post_status' => 'publish',
-      'posts_per_page' => 4,
-      'order'=>'DESC',
-    );
-    $Query = new WP_Query( $args );
-    if ($Query->have_posts()) : while ($Query->have_posts()) : $Query->the_post();
-      if( $Query->current_post == 0 ) {
-        ?>
-        <div class="col-sm-12 col-md-4 col-lg-4 mrg">
-          <div class="details-left">
-
-            <?php if ( has_post_thumbnail() ) {
-              $featured_image = get_the_post_thumbnail_url();
-            }
-            ?>
-            <a href="<?php the_permalink(); ?>"> <img src="<?php echo $featured_image; ?>" class="img-fluid"></a>
-            <div class="news-date">
-              <p><strong><?php echo strtoupper(get_post_meta(get_the_ID(), 'news_location', true));?> <?php echo get_the_date('M d, Y H:i'); ?></strong></p>
-            </div>
-            <div class="link">
-              <a href="<?php the_permalink(); ?>"><?php echo get_the_title() ?></a>
-            </div>
-          </div>
+<section class="editor_desk">
+  <section class="home-section">
+      <div class="home-copy">
+          <header class="section-header">
+              <h2 class="large-title">Editor’s Desk</h2>
+              <a class="view-all" href="#!">View More</a>
+          </header>
         </div>
-      <?php } endwhile; endif;?>
-      <div class="col-sm-12 col-md-8 col-lg-8 custom_margin">
-        <div class="col-sm-12 col-md-6 col-lg-6 remove_mob_margin">
-            <div class="details-middle">
-              <ul class="full_width_list">
-                <?php if ($Query->have_posts()) : while ($Query->have_posts()) : $Query->the_post();
-                          if( $Query->current_post != 0 ) {
-                  ?>
-                  <li>
-                    <p><strong><?php echo strtoupper(get_post_meta(get_the_ID(), 'news_location', true));?> <?php echo get_the_date('M d, Y H:i'); ?></strong></p>
-                    <div class="link">
-                      <a href="<?php the_permalink(); ?>"><?php echo get_the_title() ?></a>
-                    </div>
-                  </li>
-                  <?php
-                       }
-                endwhile; endif; ?>
-              </ul>
-            </div>
-        </div>
-        <div class="col-sm-12 col-md-6 col-lg-6 remove_mob_margin">
-          <div class="details-middle adwork">
-            <?php
-            if ( is_active_sidebar( 'news_landing_mid_banner' ) ) : ?>
-             <?php dynamic_sidebar( 'news_landing_mid_banner' ); ?>
-           <?php endif; ?>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-12 center">
-        <?php
-        if ( is_active_sidebar( 'news_landing_top_banner' ) ) : ?>
-         <?php dynamic_sidebar( 'news_landing_top_banner' ); ?>
-       <?php endif; ?>
-     </div>
-    </div>
-  </div>
-</section>
-
-<div class="wrapper">
-<section class="latest-news">
-
-<?php $count=12;foreach ($menuitems as $key => $menu) { ?>
-
-  <div id="<?php echo $menu->ID; ?>" data-anchor="<?php echo $menu->ID; ?>" class="latest-news">
-    <div class="container">
-      <?php
-        $args = array(
-        'post_type' => 'post',
-        'post_status' => 'publish',
-        'category_name' => $menu->title,
-        'posts_per_page' => 7,
-      );
-      $Query = new WP_Query( $args );
-      if ($Query->have_posts()) :
-      ?>
-      <div class="featured_headeing">
-        <h1><?php echo $menu->title; ?></h1>
-
-      </div>
-      <?php
-      endif;
-      if ($Query->have_posts()) : while ($Query->have_posts()) : $Query->the_post();
-        if( $Query->current_post == 0 ) {
-          ?>
-          <div class="col-sm-12 col-lg-5 mrg">
-            <div class="details-left">
-
-              <?php if ( has_post_thumbnail() ) {
-                $featured_image = get_the_post_thumbnail_url();
-              }
-              ?>
-
-              <a href="<?php the_permalink(); ?>"> <img src="<?php echo $featured_image; ?>" class="img-fluid"></a>
-              <div class="news-date">
-                <p><strong><?php echo strtoupper(get_post_meta(get_the_ID(), 'news_location', true));?> <?php echo get_the_date('M d, Y H:i'); ?></strong></p>
-              </div>
-
-              <div class="link">
-                <a href="<?php the_permalink(); ?>"><?php echo get_the_title() ?></a>
-              </div>
-            </div>
-          </div>
-        <?php } endwhile; endif;?>
-        <div class="col-sm-12 col-lg-7 Interview_blog mrg">
-          <div class="col-sm-12 col-lg-6 mrg">
-            <div class="details-middle devide">
-              <ul class="">
-                <?php if ($Query->have_posts()) : $counter = 0; while ($Query->have_posts()) : $Query->the_post();
-                  if ($counter <= 3) :
-                  if( $Query->current_post != 0 ){
-                  ?>
-                  <li>
-                    <p><strong><?php echo strtoupper(get_post_meta(get_the_ID(), 'news_location', true));?> <?php echo get_the_date('M d, Y H:i'); ?></strong></p>
-                    <div class="link">
-                      <a href="<?php the_permalink(); ?>"><?php echo get_the_title() ?></a>
-                    </div>
-                  </li>
-                  <?php
-                  }
-                endif; $counter++; endwhile; endif; ?>
-              </ul>
-            </div>
-          </div>
-          <div class="col-sm-12 col-lg-6 mrg newsRight_sect">
-            <div class="details-middle devide">
-              <ul class="">
-                <?php if ($Query->have_posts()) : $counter1 = 0; while ($Query->have_posts()) : $Query->the_post();
-                  if ($counter1 > 3) :
-                  if( $Query->current_post != 0 ){
-                  ?>
-                  <li>
-                    <p><strong><?php echo strtoupper(get_post_meta(get_the_ID(), 'news_location', true));?> <?php echo get_the_date('M d, Y H:i'); ?></strong></p>
-                    <div class="link">
-                      <a href="<?php the_permalink(); ?>"><?php echo get_the_title() ?></a>
-                    </div>
-                  </li>
-                  <?php
-                  }
-                endif; $counter1++; endwhile; endif; ?>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <?php if ($Query->have_posts() && ($key % 2 == 1)) :?>
-          <div class="col-lg-12 center">
-            <?php /*
-            if ( is_active_sidebar( 'news_landing_footer_banner' ) ) : ?>
-             <?php dynamic_sidebar( 'news_landing_footer_banner' ); ?>
-           <?php endif; */?>
-
-          <!-- //1055314/HT_School_Desktop_Section/HT_School_Desk_Section_Footer_Billboard-->
-            <div id='div-gpt-ad-1619596100543-<?php echo $count;?>'>
-              <script>
-                googletag.cmd.push(function() { googletag.display('div-gpt-ad-1619596100543-<?php echo $count;?>'); });
-              </script>
-            </div>
-          </div>
-        <?php $count++; endif;?>
-      </div>
-    </div>
-  <?php } ?>
-  </section>
-</div>
-  <!-- End Most Interview -->
-      <!-- <div class="container">
-        <div class="row">
-            <div class="col-lg-12 infograph">
-              <div class="heading">
-                <h4>Infographics</h4>
-              </div>
-              <div class="details mobile-slider">
+        <div class="home-copy">
+          <div class="nav-tabs-wrapper">
+            <ul class="nav nav-tabs" id="allCoursesLinks" role="tablist">
                 <?php
-                  $args1 = array(
-                      'post_type' => 'infographics',
-                      'post_status' => 'publish',
-
-                  );
-                  $Query1 = new WP_Query( $args1 );
-
-                  // print_r(get_post_custom());
-                  if ($Query1->have_posts()) : while ($Query1->have_posts()) : $Query1->the_post();
-                      $custom_fields = get_post_custom();
-                      $image_url = wp_get_attachment_url($custom_fields['image'][0]);
-
                 ?>
-                  <div class="co-sm-12 col-lg-4 item" data-aos="zoom-in" data-aos-delay="100">
-                      <a href="<?php the_permalink(); ?>"> <img src="<?php echo $image_url; ?>" class="img-fluid" /></a>
-                      <div class="content">
-                        <p><?php echo get_the_date('M d, Y H:i'); ?></p>
-                        <h3><a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a></h3>
-                      </div>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link " id="all-tab" data-toggle="tab" href="#all" role="tab" aria-controls="all" aria-selected="true">Latest</a>
+                </li>
+                <?php
+                  foreach ($menuitems as $menu) {
+                ?>  
+                <li class="nav-item" role="presentation" id="<?php echo $menu->ID; ?>">
+                    <a class="nav-link " id="<?php echo $menu->ID; ?>" data-toggle="tab" href="<?php echo $menu->url; ?>" role="tab" aria-controls="all" aria-selected="true"><?php echo $menu->title; ?></a>
+                </li>
+                <?php
+          }
+          ?>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <div class="tab-content" id="myTabContent">
+        <div class="tab-pane fade show active" id="all" role="tabpanel" aria-labelledby="all-tab">
+            <div class="home-section">
+              
+            </div>
+        </div>
+      </div>
+</section>
+<section class="home-section infographics">
+      <div class="home-copy">
+        <header class="section-header">
+            <h2 class="semi_medium-title">Infographics</h2>
+            <a class="view-all" href="#!">View More</a>
+        </header>
+      <div class="owl-carousel owl-theme student_slider">
+        <?php
+        $args1 = array(
+          'post_type' => 'testimonials',
+          'post_status' => 'publish',
+
+        );
+        $Query1 = new WP_Query( $args1 );
+        
+        if ($Query1->have_posts()) : while ($Query1->have_posts()) : $Query1->the_post();
+          $custom_fields = get_post_custom();
+          $url = wp_get_attachment_url($custom_fields['image'][0]); 
+        ?>
+          <div class="item">
+            <div class="course-card">
+              <figure class="video">
+                <img src="<?php echo $url;?>">
+                <a class="play" href="#!"><span class="time">3:20</span></a>
+              </figure>
+              <div class="course-copy">
+                <h2 class="course-title"><a href="#!"><?php echo the_content(); ?></a></h2>
+                <footer class="course-footer">
+                  <div class="left">
+                    <div class="profile">
+                      <span class="name"><?php echo $custom_fields['vibe_testimonial_author_name'][0] ?></span>
+                      <span class="position"><?php echo $custom_fields['vibe_testimonial_author_designation'][0] ?></span>
+                    </div>
                   </div>
-                <?php endwhile;endif;?>
+                  <div class="right">
+                    <a href="#!">
+                    <svg class="share" xmlns="http://www.w3.org/2000/svg" width="25.445" height="19.4" viewBox="0 0 25.445 19.4"> <g id="Group_20744" data-name="Group 20744" transform="translate(0.205 0.2)" style="isolation: isolate"> <path id="Path_38322" data-name="Path 38322" d="M21.417,21a.53.53,0,0,1,.275.133l9.091,8.188a.724.724,0,0,1,.1.919.626.626,0,0,1-.1.114l-9.091,8.188a.52.52,0,0,1-.8-.12.723.723,0,0,1-.118-.392V34.746a18.89,18.89,0,0,0-4.705.389,17.55,17.55,0,0,0-9.127,4.7.518.518,0,0,1-.8-.062.733.733,0,0,1-.113-.634C8.4,30.71,15.625,26.694,20.778,25.094V21.655a.618.618,0,0,1,.564-.66A.446.446,0,0,1,21.417,21Zm.5,1.985v2.6a.645.645,0,0,1-.426.634C17,27.53,10.737,30.858,7.913,37.407a19.292,19.292,0,0,1,7.964-3.562,21.972,21.972,0,0,1,5.5-.4.621.621,0,0,1,.542.655v2.589l7.6-6.848Z" transform="translate(-6.003 -20.995)" stroke-width="0.4"/> </g> </svg>
+                    </a>
+                  </div>
+                </footer>
               </div>
             </div>
           </div>
-        </div> -->
         <?php
+          endwhile; endif;
+        ?> 
+        </div>
+      </div>
+  </section>
+
+<section class="home-section videos">
+      <div class="home-copy">
+        <header class="section-header">
+            <h2 class="semi_medium-title">Videos</h2>
+            <a class="view-all" href="#!">View More</a>
+        </header>
+      <div class="owl-carousel owl-theme student_slider">
+        <?php
+        $args1 = array(
+          'post_type' => 'testimonials',
+          'post_status' => 'publish',
+
+        );
+        $Query1 = new WP_Query( $args1 );
+        
+        if ($Query1->have_posts()) : while ($Query1->have_posts()) : $Query1->the_post();
+          $custom_fields = get_post_custom();
+          $url = wp_get_attachment_url($custom_fields['image'][0]); 
+        ?>
+          <div class="item">
+            <div class="course-card">
+              <figure class="video">
+                <img src="<?php echo $url;?>">
+                <a class="play" href="#!"><span class="time">3:20</span></a>
+              </figure>
+              <div class="course-copy">
+                <h2 class="course-title"><a href="#!"><?php echo the_content(); ?></a></h2>
+                <footer class="course-footer">
+                  <div class="left">
+                    <div class="profile">
+                      <span class="name"><?php echo $custom_fields['vibe_testimonial_author_name'][0] ?></span>
+                      <span class="position"><?php echo $custom_fields['vibe_testimonial_author_designation'][0] ?></span>
+                    </div>
+                  </div>
+                  <div class="right">
+                    <a href="#!">
+                    <svg class="share" xmlns="http://www.w3.org/2000/svg" width="25.445" height="19.4" viewBox="0 0 25.445 19.4"> <g id="Group_20744" data-name="Group 20744" transform="translate(0.205 0.2)" style="isolation: isolate"> <path id="Path_38322" data-name="Path 38322" d="M21.417,21a.53.53,0,0,1,.275.133l9.091,8.188a.724.724,0,0,1,.1.919.626.626,0,0,1-.1.114l-9.091,8.188a.52.52,0,0,1-.8-.12.723.723,0,0,1-.118-.392V34.746a18.89,18.89,0,0,0-4.705.389,17.55,17.55,0,0,0-9.127,4.7.518.518,0,0,1-.8-.062.733.733,0,0,1-.113-.634C8.4,30.71,15.625,26.694,20.778,25.094V21.655a.618.618,0,0,1,.564-.66A.446.446,0,0,1,21.417,21Zm.5,1.985v2.6a.645.645,0,0,1-.426.634C17,27.53,10.737,30.858,7.913,37.407a19.292,19.292,0,0,1,7.964-3.562,21.972,21.972,0,0,1,5.5-.4.621.621,0,0,1,.542.655v2.589l7.6-6.848Z" transform="translate(-6.003 -20.995)" stroke-width="0.4"/> </g> </svg>
+                    </a>
+                  </div>
+                </footer>
+              </div>
+            </div>
+          </div>
+        <?php
+          endwhile; endif;
+        ?> 
+        </div>
+      </div>
+  </section>
+
+   <?php
         get_footer(vibe_get_footer());
         ?>

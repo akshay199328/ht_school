772a82427401ccb0a5bdee30f7451e513d26be6e
Noman Sayyed <sayyednoman@gmail.com>
2021-09-09 13:49:21 +0530

    bookmark

--- code/wp-content/themes/htschools-v1/static-pages.php
+++ code/wp-content/themes/htschools-v1/static-pages.php
@@ -16,58 +16,42 @@ if(vibe_validate($title) || empty($title)){
 ?>
 <section id="content" class="static_content grey-background">
     <div class="<?php echo vibe_get_container(); ?>">
-        <div class="row">
-            <div class="col-md-12 ">
-              <div class="breadcrumbs">
-                  <?php
-                      $breadcrumbs=get_post_meta(get_the_ID(),'vibe_breadcrumbs',true);
-                      if(vibe_validate($breadcrumbs) || empty($breadcrumbs))
-                          vibe_breadcrumbs(); 
-                  ?>
-                  
-                  <?php the_sub_title(); ?>
-              </div>
+      <nav aria-label="breadcrumb" class="breadcrumb-wrapper">
+         <?php
+            $breadcrumbs=get_post_meta(get_the_ID(),'vibe_breadcrumbs',true);
+            if(vibe_validate($breadcrumbs) || empty($breadcrumbs))
+                vibe_breadcrumbs(); 
+         ?>
+         <?php the_sub_title(); ?>
+      </nav>
+      <div class="content-copy">
+         <div class="sidebar">
+            <ul class="border-link left_tab course-listslider">
+            <?php
+            $menu_name = 'footer-menu'; //menu slug
+            $locations = get_nav_menu_locations();
+            $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
+            $menuitems = wp_get_nav_menu_items( $menu->term_id, array( 'order' => 'DESC' ) );
+            //print_r($menuitems);
+            foreach ($menuitems as $menu) { 
+            $current = ( $_SERVER['REQUEST_URI'] == parse_url( $menu->url, PHP_URL_PATH ) ) ? 'active' : ''; 
+            if($menu->title != 'Contact Us'){
+            echo '<li class="' . $current . ' item"><a href="' . $menu->url . '">' . $menu->title . '</a></li>';
+            }
+            }
+            ?>
+            </ul>
+         </div>
+         <div class="right-copy">
+         <div class="content">
+            <div class="pagetitle">
+               <h1><?php the_title(); ?></h1> 
             </div>
-            <div class="details">
-                <div class="col-md-3 col-sm-12">
-                    <div class="sidebar">
-                       <ul class="border-link left_tab course-listslider">
-                            <?php
-                               $menu_name = 'footer-menu'; //menu slug
-                               $locations = get_nav_menu_locations();
-                               $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
-                               $menuitems = wp_get_nav_menu_items( $menu->term_id, array( 'order' => 'DESC' ) );
-                               //print_r($menuitems);
-                               foreach ($menuitems as $menu) { 
-                               $current = ( $_SERVER['REQUEST_URI'] == parse_url( $menu->url, PHP_URL_PATH ) ) ? 'active' : ''; 
-                                if($menu->title != 'Contact Us'){
-                                   echo '<li class="' . $current . ' item"><a href="' . $menu->url . '">' . $menu->title . '</a></li>';
-                                }
-                               }
-                           ?>
-
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-md-9 col-sm-12">
-                    <div class="content">
-                      <div class="pagetitle">
-                        <h1><?php the_title(); ?></h1> 
-                      </div>
-                      
-                        <?php
-                            the_content();
-                           
-                         ?>
-                    </div>
-                    <?php
-                    
-                    endwhile;
-                    endif;
-                    ?>
-                </div>
-            </div>
-        </div>
+            <?php the_content(); ?>
+         </div>
+         <?php endwhile; endif; ?>
+         </div>
+      </div>
     </div>
 </section>
 

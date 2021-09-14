cf55abe31fcea20b37bb10bd8551d6e4e5755f35
bhavnaFortune4 <bhavna.langi@fortune4.in>
2021-09-14 10:35:54 +0530

    UI Changes

--- code/wp-content/themes/htschools-v1/testimonial-listing.php
+++ code/wp-content/themes/htschools-v1/testimonial-listing.php
@@ -92,7 +92,31 @@ get_header(vibe_get_header());
                                 <p><?php echo the_content();?></p>
                             </div>
                         </div>
+                        <footer class="course-footer">
+                      <div class="right">
+                         
+                          <a href="<?php echo get_bloginfo('template_url');?>" class="hover_share">
+                              <svg xmlns="http://www.w3.org/2000/svg" width="25.445" height="19.4" viewBox="0 0 25.445 19.4">
+                                  <g id="Group_21136" data-name="Group 21136" transform="translate(0.205 0.2)" style="isolation: isolate">
+                                      <path id="Path_38322" data-name="Path 38322" d="M21.417,21a.53.53,0,0,1,.275.133l9.091,8.188a.724.724,0,0,1,.1.919.626.626,0,0,1-.1.114l-9.091,8.188a.52.52,0,0,1-.8-.12.723.723,0,0,1-.118-.392V34.746a18.89,18.89,0,0,0-4.705.389,17.55,17.55,0,0,0-9.127,4.7.518.518,0,0,1-.8-.062.733.733,0,0,1-.113-.634C8.4,30.71,15.625,26.694,20.778,25.094V21.655a.618.618,0,0,1,.564-.66A.446.446,0,0,1,21.417,21Zm.5,1.985v2.6a.645.645,0,0,1-.426.634C17,27.53,10.737,30.858,7.913,37.407a19.292,19.292,0,0,1,7.964-3.562,21.972,21.972,0,0,1,5.5-.4.621.621,0,0,1,.542.655v2.589l7.6-6.848Z" transform="translate(-6.003 -20.995)" fill="#ff5a5a" stroke="#ff5a5a" stroke-width="0.4"/>
+                                  </g>
+                              </svg>
+                              <!-- <div class="display_icon">
+                                  <h6>Share <span><i class="bi bi-x close-share"></i></span></h6>
+                                  <div class="a2a_kit a2a_kit_size_32 a2a_default_style" data-a2a-url="<?php echo get_bloginfo('url')?>/<?php echo $post->post_name;?>" data-a2a-title="<?php echo $post->post_title. ' - '.get_bloginfo(); ?>" data-id="<?php echo $post->ID;?>">
+                                      <a class="a2a_button_facebook"></a>
+                                      <a class="a2a_button_twitter"></a>
+                                      <a class="a2a_button_pinterest"></a>
+                                      <a class="a2a_button_google_gmail"></a>
+                                      <a class="a2a_button_whatsapp"></a>
+                                      <a class="a2a_button_telegram"></a>
+                                  </div><script async src="https://static.addtoany.com/menu/page.js"></script>
+                              </div> -->
+                          </a>
+                      </div>
+                  </footer>
                     </div>
+
                 </div>
             </div>
       </div>
